49b962b12fb082900dc83eb439098fa18f3caa1f
bilal <azbk2004@gmail.com>
2025-02-20 20:13:41 +0600

    add update car logic and delete car logic

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.index_view, name='index'),
     path('news_create/', views.news_create_view, name='news_create'),
     path('car_create/', views.car_create_view),
+    path('car_detail/<int:pk>/', views.car_detail_view, name='detail'),
     path('car_create_2/', views.car_create_view_2),
+    path('car_detail_2/<int:pk>/', views.car_detail_view_2, name='detail_2'),
+    path('car_delete/<int:pk>/', views.car_delete_view, name='delete')
 ]
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.db.models import Q
 
 from .models import News, Category, Color, Car
-from .forms import CarCreateForm
+from .forms import CarForm
 
 
 def index_view(request):
@@ -56,12 +56,65 @@ def car_create_view(request):
 def car_create_view_2(request):
 
     if request.method == 'POST':
-        form = CarCreateForm(request.POST, request.FILES)
+        form = CarForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect("index")
 
-    form = CarCreateForm()
+    form = CarForm()
 
     return render(request, template_name='app/car_create_2.html', context={'form': form})
 
+
+def car_detail_view(request, pk):
+    car = Car.objects.get(id=pk)
+
+    if request.method == 'POST':
+        form = CarForm(request.POST, request.FILES, instance=car)
+        if form.is_valid():
+            form.save()
+            return redirect("detail", car.id)
+
+    form = CarForm(instance=car)
+
+    return render(request, 'app/car_detail.html', {'car': car, 'form': form})
+
+
+def car_detail_view_2(request, pk):
+    categories = Category.objects.all()
+    colors = Color.objects.all()
+    car = Car.objects.get(id=pk)
+
+    if request.method == 'POST':
+        title = request.POST['title']
+        model = request.POST['model']
+        year = request.POST['year']
+        odometer = request.POST['odometer']
+        engine_capacity = request.POST['engine_capacity']
+        color_id = request.POST['color_id']
+        category_id = request.POST['category_id']
+        image = request.FILES['image']
+
+        category = Category.objects.get(id=category_id)
+        color = Color.objects.get(id=color_id)
+
+        car.title = title
+        car.model = model
+        car.year = year
+        car.odometer = odometer
+        car.engine_capacity = engine_capacity
+        car.color_id = color
+        car.category_id = category
+        car.image = image
+        car.save()
+        return redirect('index')
+
+    return render(request=request, template_name='app/car_detail_2.html', context={'car': car, 'categories': categories,
+                                                                                 'colors': colors})
+
+
+def car_delete_view(request, pk):
+    car = Car.objects.get(id=pk)
+    car.delete()
+
+    return redirect('index')
